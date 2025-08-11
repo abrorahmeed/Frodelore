a852b8b731736da679bae50289ef7dc428f9ae1a
abrorahmeed <abrorahmeed@gmail.com>
2025-08-11 12:04:57 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('hamburger').addEventListener('click', function() {
+  const navMenu = document.getElementById('navMenu');
+  navMenu.style.display = navMenu.style.display === 'block' ? 'none' : 'block';
+});
