61dc0aafaafc2c0efdf84761decb3654f62e2d0a
abrorahmeed <abrorahmeed@gmail.com>
2025-08-11 12:13:54 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-document.getElementById('hamburger').addEventListener('click', function() {
-  const navMenu = document.getElementById('navMenu');
-  navMenu.style.display = navMenu.style.display === 'block' ? 'none' : 'block';
+const hamburger = document.getElementById('hamburger');
+const mobileMenu = document.getElementById('mobileMenu');
+
+hamburger.addEventListener('click', () => {
+  mobileMenu.style.display = mobileMenu.style.display === 'flex' ? 'none' : 'flex';
 });
